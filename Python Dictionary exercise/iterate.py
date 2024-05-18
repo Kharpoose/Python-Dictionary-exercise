af0446453f59a2c09a68eabeba0fbc603f04250a
Çağrı <karpuzcagri@gmail.com>
2024-05-18 22:17:07 +0900

    main

---
+++ Python Dictionary exercise/iterate.py
@@ -0,0 +1,4 @@
+x = {'Red': 1, 'Green': 2, 'Blue': 3}
+
+for color_key, value in x.items():
+    print(color_key, 'corresponds to ', value)
---
+++ Python Dictionary exercise/sum_in_the_list.py
@@ -0,0 +1,6 @@
+my_dict = {'data1': 98.13, 'data2': -54.44, 'data3': 360.2}
+
+
+x = sum(my_dict.values())
+
+print(x)
